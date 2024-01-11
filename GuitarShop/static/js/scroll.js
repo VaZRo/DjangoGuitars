8172bb84b0408dd312d3a3c632610e6ce594f374
VaZRo <vazro053@gmail.com>
2024-01-11 22:44:39 +0600

    Add adaptive for home page

--- GuitarShop/static/js/scroll.js
+++ GuitarShop/static/js/scroll.js
@@ -1,40 +1,30 @@
-$(document).ready(function() {
-    $(".metal .guitars.slider").slick({
-        prevArrow: $(".metal .button-left"),
-        nextArrow: $(".metal .button-right"),
+$(document).ready(function () {
+    $(".carousel .guitars.slider").slick({
+        prevArrow: $(".carousel .button-left"),
+        nextArrow: $(".carousel .button-right"),
         dots: false,
         infinite: true,
-        speed: 500,
         slidesToShow: 3,
-        slidesToScroll: 2,
+        slidesToScroll: 3,
+        speed: 500,
         autoplay: true,
         autoplaySpeed: 5000,
         cssEase: 'ease-in-out',
-      });
-
-      $(".classic .guitars.slider").slick({
-        prevArrow: $(".classic .button-left"),
-        nextArrow: $(".classic .button-right"),
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 2,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        cssEase: 'ease-in-out',
-      });
-
-      $(".stomps .guitars.slider").slick({
-        prevArrow: $(".stomps .button-left"),
-        nextArrow: $(".stomps .button-right"),
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 5000,
-        cssEase: 'ease-in-out',
-      });
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+        ]
+    });
 })
