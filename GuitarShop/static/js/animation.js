d531cb25173657d332a8598e98389225da08a216
VaZRo <vazro053@gmail.com>
2024-01-13 00:14:29 +0600

    added product page

--- GuitarShop/static/js/animation.js
+++ GuitarShop/static/js/animation.js
@@ -1,7 +1,7 @@
-$(window).scroll(function() {
-  if ($(this).scrollTop() > 0) {
-    $('.menu').addClass('sticky');
-  } else {
-    $('.menu').removeClass('sticky');
-  }
-});
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 0) {
+//     $('.menu').addClass('sticky');
+//   } else {
+//     $('.menu').removeClass('sticky');
+//   }
+// });
