6a82729fa2a2eb1bc280cae2e12d850e69b6ffe5
VaZRo <vazro053@gmail.com>
2024-01-21 19:23:37 +0600

    MWP is done

--- GuitarShop/static/js/basket.js
+++ GuitarShop/static/js/basket.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     $('.basket_list').on('click', 'input[type="number"]', function(ev){
         let t_href = ev.target;
-
+        console.log(t_href);
         $.ajax({
             url: '/basket/edit/' + t_href.name + '/' + t_href.value + '/',
             success: function (data){
